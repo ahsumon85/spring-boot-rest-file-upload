eab5e4863b6e8aaa733f79f8fbe96578aa8f614f
Ahasan Habib <habibsumoncse@gmail.com>
2020-10-30 12:21:39 +0600

    add project

---
+++ src/main/java/com/ahasan/file/SpringBootFileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.ahasan.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFileUploadApplication.class, args);
+	}
+
+}
