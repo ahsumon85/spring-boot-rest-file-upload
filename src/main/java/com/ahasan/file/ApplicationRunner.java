abdaf69e1c624b9310095157fc82cc7d03d59951
Ahasan Habib <habibsumoncse@gmail.com>
2020-11-21 11:30:40 +0600

    update project configuration

--- src/main/java/com/ahasan/file/ApplicationRunner.java
+++ src/main/java/com/ahasan/file/ApplicationRunner.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootFileUploadApplication {
+public class ApplicationRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootFileUploadApplication.class, args);
+		SpringApplication.run(ApplicationRunner.class, args);
+		System.out.println("Spring boot application running.....");
 	}
 
 }
